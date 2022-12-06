aafc14c4fc11601144be4b7d5e26c4830fb1cf94
masroorkazarani <119839620+masroorkazarani@users.noreply.github.com>
2022-12-06 20:00:14 +0530

    #include<stdio.h> int main(){     int n,rem,sum=0,pro=1,diff;     scanf("%d",&n);     while(n>0){         rem=n%10;         sum=sum+rem;         pro=pro*rem;         n=n/10;     }     if(pro>sum) printf("%d",pro-sum);     else printf("%d",sum-pro); }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,rem,sum=0,pro=1,diff;
+    scanf("%d",&n);
+    while(n>0){
+        rem=n%10;
+        sum=sum+rem;
+        pro=pro*rem;
+        n=n/10;
+    }
+    if(pro>sum) printf("%d",pro-sum);
+    else printf("%d",sum-pro);
+}
